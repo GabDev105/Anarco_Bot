b78dd686a9915ff1099d09ed2a1a4d913f91f31c
GabDev105 <68088309+GabDev105@users.noreply.github.com>
2020-07-09 21:56:48 -0500

    Update Anarco_Bot.py

--- Anarco_Bot.py
+++ Anarco_Bot.py
@@ -3,17 +3,14 @@ import time
 import amanobot
 from amanobot.loop import MessageLoop
 
-TOKEN = '1173665239:AAEmGc8HMTq3J5EmDLN_75g2cfar55co7Ok'
+TOKEN = 'TOKEN'
 
 def handle(msg):
     content_type, chat_type, chat_id = amanobot.glance(msg)
     ##print(bot.getUpdates())
-    if content_type == 'text': msgtext = msg['text']
-    else: msgtext = "Vazio"
-    print("=======================================\nTipo de Chat: {}\nNome: {}\nTipo: {}\nMensagem: {}\n=======================================\n".format(msg['chat']['type'], msg['from']['first_name'], content_type, msgtext))
 
     if msg['text'] == '/start':
-        bot.sendMessage(chat_id,"Olá {}, Meu nome é AnarcoBotⒶ.\n"
+        bot.sendMessage(chat_id,"Olá {}, Meu nome é AncapBotⒶ.\n"
                                 "Meu objetivo é descentralizar a informação dispersa sobre o Libertarianismo.\n"
                                 "Estou aqui para ajudá-lo se tiver dúvidas sob o tema!\n"
                                 "Tenho muitos comandos úteis e um fluxograma (/QuemFaria) para acabar com as perguntas:\n"
@@ -32,6 +29,14 @@ def handle(msg):
 "/Livros: Recomendações de leituras para iniciar no Libertarianismo.\n"
 "/About: Mostrar mais informações sobre o processo e apoio no desenvolvimento do bot.\n".format(msg['from']['username']))
     
+    if msg['text'] == '/Defs':
+        bot.sendMessage(chat_id,"Próximamente...".format(msg['from']['username']))
+
+
+
+
+
+
     if msg['text'].split(' ')[0] == '/echo':
         bot.sendMessage(chat_id, msg[u'text'].split(' ', 1)[1])
 
