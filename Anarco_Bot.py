6a94ecc4aa4db02fc2e5cedf06ef42da9e2987d0
GabDev105 <68088309+GabDev105@users.noreply.github.com>
2020-07-09 21:42:28 -0500

    Update Anarco_Bot.py

--- Anarco_Bot.py
+++ Anarco_Bot.py
@@ -1,83 +1,42 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# This program is dedicated to the public domain under the CC0 license.
+import sys
+import time
+import amanobot
+from amanobot.loop import MessageLoop
 
-"""
-Bot para responder certas questões sobre Libertarianismo.
+TOKEN = '1173665239:AAEmGc8HMTq3J5EmDLN_75g2cfar55co7Ok'
 
-Primeiro, algumas funções do manipulador são definidas. Em seguida, essas funções são passadas para
-expedidor e registrado em seus respectivos locais.
-Em seguida, o bot é iniciado e executado até pressionar Ctrl-C na linha de comando.
+def handle(msg):
+    content_type, chat_type, chat_id = amanobot.glance(msg)
+    ##print(bot.getUpdates())
+    if content_type == 'text': msgtext = msg['text']
+    else: msgtext = "Vazio"
+    print("=======================================\nTipo de Chat: {}\nNome: {}\nTipo: {}\nMensagem: {}\n=======================================\n".format(msg['chat']['type'], msg['from']['first_name'], content_type, msgtext))
 
-Uso:
-Responde comandos já estabelecidos.
-Pressione Ctrl-C na linha de comando ou envie um sinal ao processo para interromper o
-robô.
-"""
+    if msg['text'] == '/start':
+        bot.sendMessage(chat_id,"Olá {}, Meu nome é AnarcoBotⒶ.\n"
+                                "Meu objetivo é descentralizar a informação dispersa sobre o Libertarianismo.\n"
+                                "Estou aqui para ajudá-lo se tiver dúvidas sob o tema!\n"
+                                "Tenho muitos comandos úteis e um fluxograma (/QuemFaria) para acabar com as perguntas:\n"
+                                "\n"
+                                "'¿Mas, sem o estado quem faria as estradas?'\n"
+                                "'¿Quem forneceria saúde, educação e segurança?'\n"
+                                "\n"
+                                "Para inicar meus comandos, clique em /help\n"
+                                "Salve galera, tmj🐍.".format(msg['from']['username']))
 
-import logging
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text("Olá! Meu nome é AnarcoBotⒶ.\n"
-    "Meu objetivo é descentralizar a informação dispersa sobre o Libertarianismo.\n" 
-    "Estou aqui para ajudá-lo se tiver dúvidas sob o tema!\n"
-    "Tenho muitos comandos úteis e um fluxograma para acabar com as perguntas:\n"
-    "\n" 
-    "'¿Mas, sem o estado quem faria as estradas?'\n"
-    "'¿Quem forneceria saúde, educação e segurança?'\n"
-    "\n"
-    "Para inicar meus comandos, clique em /help\n"
-    "Salve galera, tmj🐍.")
-
-
-def help_command(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text("/Defs: Mostrar lista de comandos de termos libertários e suas respectivas definições.\n"
+    if msg['text'] == '/help':
+        bot.sendMessage(chat_id,"/Defs: Mostrar lista de comandos de termos libertários e suas respectivas definições.\n"
 "/QuemFaria: Iniciar fluxograma sobre a produção de qualquer serviço ou mercadoria.\n"
 "/Cryptos: Mostrar comandos com resumos breves de criptomoedas com utilidade para o nosso agorismo do dia a dia.\n"
 "/Canais: Mostrar uma quantidade sustancial de canais de viés libertário.\n"
 "/Livros: Recomendações de leituras para iniciar no Libertarianismo.\n"
-"/About: Mostrar mais informações sobre o processo e apoio no desenvolvimento do bot.\n")
+"/About: Mostrar mais informações sobre o processo e apoio no desenvolvimento do bot.\n".format(msg['from']['username']))
+    
+    if msg['text'].split(' ')[0] == '/echo':
+        bot.sendMessage(chat_id, msg[u'text'].split(' ', 1)[1])
 
+bot = amanobot.Bot(TOKEN)
+MessageLoop(bot, handle).run_as_thread()
 
-
-
-
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater("1173665239:AAEmGc8HMTq3J5EmDLN_75g2cfar55co7Ok", use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help_command))
-
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+while 1:
+    time.sleep(2)
